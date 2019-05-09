eb960131afb4b8ff8687c62b7591ba9cc17cb90f
loumac91 <loumac91@googlemail.com>
2019-05-09 13:27:20 +0100

    kvp.js

---
+++ kvp.js
@@ -0,0 +1,19 @@
+const test = {
+    x: '123',
+    y: '456',
+    abc: '789'
+}
+
+const entries = Object.entries(ttest)
+
+for (const [key, value] of entries) {
+    console.log(`The value of ${key} is ${value}`)
+}
+
+const result = Object.fromEntries(
+    Object.entries(test)
+        .filter(([key, value]) => key.length === 1)
+        .map(([key, value]) => [ key, value * 2])
+)
+
+console.log('result', result)
